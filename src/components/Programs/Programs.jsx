d1754af7f8d8a6fb70cf74da9577e57fb87cca39
manoj-098 <manojvenki098@gmail.com>
2024-02-10 09:26:59 +0530

    Third Commit

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -12,7 +12,7 @@ const Programs = () => {
             <span className='stroke-text'>TO SHAPE YOU</span>
         </div>
         <div className="programs-data">
-            {/* {programsData.map((program,index)=>{return(
+            {programsData.map((program,index)=>{return(
             <div className='data-description' key={index}>
                 {program.image}
                 <span>{program.heading}</span>
@@ -22,8 +22,8 @@ const Programs = () => {
                     <img src={Arrow} alt=""></img>
                 </div>
             </div>
-            )})} */}
-            <div className="data-description">
+            )})}
+            {/* <div className="data-description">
                 <img src={Arrow} alt="" />
                 <span>PROGRAM HEADING</span>
                 <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos esse facilis voluptatem aspernatur eveniet laborum tempore explicabo fuga unde beatae?</span>
@@ -58,7 +58,7 @@ const Programs = () => {
                     <span>Join now</span>
                     <img src={Arrow} alt="" />
                 </div>
-            </div>
+            </div> */}
         </div>
     </div>
   )
