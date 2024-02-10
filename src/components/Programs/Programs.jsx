a026a92b296692108bd713f674a8526c23b79020
manoj-098 <manojvenki098@gmail.com>
2024-02-10 09:02:18 +0530

    Second Commit

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -12,7 +12,7 @@ const Programs = () => {
             <span className='stroke-text'>TO SHAPE YOU</span>
         </div>
         <div className="programs-data">
-            {programsData.map((program,index)=>{return(
+            {/* {programsData.map((program,index)=>{return(
             <div className='data-description' key={index}>
                 {program.image}
                 <span>{program.heading}</span>
@@ -22,7 +22,43 @@ const Programs = () => {
                     <img src={Arrow} alt=""></img>
                 </div>
             </div>
-            )})}
+            )})} */}
+            <div className="data-description">
+                <img src={Arrow} alt="" />
+                <span>PROGRAM HEADING</span>
+                <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos esse facilis voluptatem aspernatur eveniet laborum tempore explicabo fuga unde beatae?</span>
+                <div className="join-now">
+                    <span>Join now</span>
+                    <img src={Arrow} alt="" />
+                </div>
+            </div>
+            <div className="data-description">
+                <img src={Arrow} alt="" />
+                <span>PROGRAM HEADING</span>
+                <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos esse facilis voluptatem aspernatur eveniet laborum tempore explicabo fuga unde beatae?</span>
+                <div className="join-now">
+                    <span>Join now</span>
+                    <img src={Arrow} alt="" />
+                </div>
+            </div>
+            <div className="data-description">
+                <img src={Arrow} alt="" />
+                <span>PROGRAM HEADING</span>
+                <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos esse facilis voluptatem aspernatur eveniet laborum tempore explicabo fuga unde beatae?</span>
+                <div className="join-now">
+                    <span>Join now</span>
+                    <img src={Arrow} alt="" />
+                </div>
+            </div>
+            <div className="data-description">
+                <img src={Arrow} alt="" />
+                <span>PROGRAM HEADING</span>
+                <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos esse facilis voluptatem aspernatur eveniet laborum tempore explicabo fuga unde beatae?</span>
+                <div className="join-no">
+                    <span>Join now</span>
+                    <img src={Arrow} alt="" />
+                </div>
+            </div>
         </div>
     </div>
   )
